e39ee80cc9b0918399941f5c5cfabc14f896f064
Yutong Liang <yliang6@cs.brown.edu>
2020-04-19 15:01:25 -0400

    bug fix

--- main.py
+++ main.py
@@ -19,12 +19,15 @@ def main():
 	# load images, which are stored in a folder named 'rectified' in the same directory as this file
 	data_dir = os.path.dirname(os.path.abspath(__file__))
 	ic = io.collection.ImageCollection(data_dir+'/rectified/*.png')
-	left_ic = ic[:6509]
-	right_ic = ic[6510:]
+	half_idx = len(ic)//2
+	left_ic = ic[:half_idx]
+	right_ic = ic[half_idx:]
 	# images have shape (480, 640, 3)
-	for i in range(len(left_ic)):
-		img1 = left_ic[i]
-		img2 = right_ic[i]
+	for i in range(len(left_ic)-1):
+		imgl1 = left_ic[i]
+		imgr1 = right_ic[i]
+		imgl2 = left_ic[i+1]
+		imgr2 = right_ic[i+1]
 		# display left and right images for verification purposes
 		'''
 		curr = [left_ic[i],right_ic[i]]
@@ -32,11 +35,11 @@ def main():
 		plt.show()
 		'''
 		# frame1
-		matchesl1, matchesr1 = matchFeatures(img1, img2)
+		matchesl1, matchesr1 = matchFeatures(imgl1, imgr1)
 		matchesl1, matchesr1 = RANSAC(matchesl1, matchesr1)
 		coords3d1 = triangulate(matchesl1, matchesr1)
 		# frame2
-		matchesl2, matchesr2 = matchFeatures(img1, img2)
+		matchesl2, matchesr2 = matchFeatures(imgl2, imgr2)
 		matchesl2, matchesr2 = RANSAC(matchesl2, matchesr2)
 		coords3d2 = triangulate(matchesl2, matchesr2)
 		updateCameraPose(coords3d1, coords3d2)

7d487ffc4d227749f4e75379341437497857e74f
Alex Wey <53589584+alex-wey@users.noreply.github.com>
2020-04-26 01:12:16 -0400

    Update main.py

--- main.py
+++ main.py
@@ -9,16 +9,13 @@ from update_camera_pose import update_camera_pose
 from update_camera_pose import ransac_F_Matrix
 from triangulate import triangulate
 from match_features import match_features
+from gif import gif
 
-<<<<<<< HEAD
 # camera poses array
 plot_C = np.zeros((6000, 3))
 
 # initial camera pose
 C = np.diag(np.ones(4))
-=======
-
->>>>>>> dced6c939532689fa1c3ffdabdde7e0f60e89b11
 
 def main():
 	# load images, which are stored in a folder named 'rectified' in the same directory as this file
@@ -46,22 +43,22 @@ def main():
 		io.imshow_collection(curr)
 		plt.show()
 		'''
-		# frame1
 		matchesl1, matchesr1, matchesl2, matchesr2 = match_features(imgl1, imgr1, imgl2, imgr2)
-		F1, inliers_a1, inliers_b1 = ransac_F_Matrix(matchesl1, matchesr1)
-		coords3d1 = triangulate(inliers_a1, inliers_b1)
+
+		# frame1
+		# F1, inliers_a1, inliers_b1 = ransac_F_Matrix(matchesl1, matchesr1)
+		# coords3d1 = triangulate(inliers_a1, inliers_b1)
+		coords3d1 = triangulate(matchesl1, matchesr1)
+		
 		# frame2
-		# matchesl2, matchesr2 = match_features(imgl2, imgr2)
-		F2, inliers_a2, inliers_b2 = ransac_F_Matrix(matchesl2, matchesr2)
-		coords3d2 = triangulate(inliers_a2, inliers_b2)
+		# F2, inliers_a2, inliers_b2 = ransac_F_Matrix(matchesl2, matchesr2)
+		# coords3d2 = triangulate(inliers_a2, inliers_b2)
+		coords3d2 = triangulate(matchesl2, matchesr2)
 		
 		C = update_camera_pose(coords3d1, coords3d2, C)
-<<<<<<< HEAD
+		plot_C[i] = C[0:3,3].T
 
-=======
->>>>>>> dced6c939532689fa1c3ffdabdde7e0f60e89b11
-		print(C)
-
+	gif(plot_C[0:200])
 
 
 main()

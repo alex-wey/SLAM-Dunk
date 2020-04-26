5ca44a0803a358152a1df373212a6a7b26a9b95b
Alex Wey <53589584+alex-wey@users.noreply.github.com>
2020-04-26 01:11:50 -0400

    Update gif.py

--- gif.py
+++ gif.py
@@ -15,18 +15,21 @@ def gif(matrix):
 
     gif_coords = np.zeros((matrix.shape[0], matrix.shape[1]))
 
+    ax = plt.figure().add_subplot(111, projection='3d')
+    ax.set_xlim3d(np.min(matrix[:,0])-10, np.max(matrix[:,0])+10)
+    ax.set_ylim3d(np.min(matrix[:,1])-10, np.max(matrix[:,1])+10)
+    ax.set_zlim3d(np.min(matrix[:,2])-10, np.max(matrix[:,2])+10)
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+    ax.set_zlabel('z')
+
     for i, coords in enumerate(matrix):
-        
-        ax = plt.figure().add_subplot(111, projection='3d')
-        ax.set_zlim3d(0, 35)
-        ax.set_ylim3d(0, 35)
-        ax.set_xlim3d(0, 35)
-        ax.set_xlabel('x')
-        ax.set_ylabel('y')
-        ax.set_zlabel('z')
 
         gif_coords[i] = coords
-        x, y, z = np.hsplit(gif_coords[0:i+1], 3)
+        x = gif_coords[0:i+1,0]
+        y = gif_coords[0:i+1,1]
+        z = gif_coords[0:i+1,2]
+        
         ax.scatter(x, y, z)
         plt.savefig('poses/camera_pose_' + str(i) + '.png')
 
@@ -36,11 +39,11 @@ def gif(matrix):
 
 def main():
 
-    coords = np.zeros((30, 3))
-    c = np.array([1, 2, 3])
-    for i in range(30):
-        coords[i] = c
-        c = np.add(c, 1)
+    # coords = np.zeros((30, 3))
+    # c = np.array([1, 2, 3])
+    # for i in range(30):
+    #     coords[i] = c
+    #     c = np.add(c, 1)
 
     gif(coords)
 

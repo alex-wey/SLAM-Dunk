6305b53f744db3dfaa1b3c920ba9c2e858ec1042
이청 <leechung@icheong-ui-MacBook-Pro.local>
2020-05-04 07:41:47 +0900

    add

--- match_features.py
+++ match_features.py
@@ -2,11 +2,26 @@ import numpy as np
 import cv2
 from matplotlib import pyplot as plt
 from skimage.color import rgb2gray
+import itertools
+from PIL import Image
+
 
 #Input: two images
 #Output: two (n, 2) matrix matches
+#$ pip install image_slicer
+
 def match_features(imgl, imgr, imgln, imgrn):
+    #imgl, imgr = k-1 pairs  imgln, imgrn = k pairs
     print("Matching Features...")
+
+    itr = 0
+    width1 = 240
+    width2 = 320
+
+    # imgl = cv2.imread('wean_wide_interesting.left-rectified.00000600.t_001268594688.921905.png')
+    # imgr = cv2.imread('wean_wide_interesting.left-rectified.00000601.t_001268594688.992430.png')
+    # imgln = cv2.imread('wean_wide_interesting.right-rectified.00000600.t_001268594688.921905.png')
+    # imgrn = cv2.imread('wean_wide_interesting.right-rectified.00000601.t_001268594688.992430.png')
 
     gray_imgl = cv2.cvtColor(imgl, cv2.COLOR_BGR2GRAY)
     gray_imgr = cv2.cvtColor(imgr, cv2.COLOR_BGR2GRAY)
@@ -15,60 +30,126 @@ def match_features(imgl, imgr, imgln, imgrn):
 
     sift = cv2.xfeatures2d.SIFT_create()
 
-    k1, d1 = sift.detectAndCompute(gray_imgl, None)
-    k2, d2 = sift.detectAndCompute(gray_imgr, None)
-    k1n, d1n = sift.detectAndCompute(gray_imgln, None)
-    k2n, d2n = sift.detectAndCompute(gray_imgrn, None)
+    total_lmatches = []
+    total_rmatches = []
+    total_lmatchesn = []
+    total_rmatchesn = []
+
+    row, col = gray_imgl.shape
+
+    for i in range(0,row, width1):
+        for j in range(0, col, width2):
+            chop1 = gray_imgl[i : i +width1, j:j+width2]
+            chop2 = gray_imgr[i : i +width1, j:j+width2]
+            chop3 = gray_imgln[i : i +width1, j:j+width2]
+            chop4 = gray_imgrn[i : i +width1, j:j+width2]
 
 
-    bf = cv2.BFMatcher(cv2.NORM_L2, crossCheck=True)
-
-    #matches between imgl and imgln
-    matches1n = bf.match(d1,d1n)
-    #matches between imgl and imgr
-    matches2 = bf.match(d1,d2)
-    #matches between imgl and imgrn
-    matches2n = bf.match(d1,d2n)
+            chop1 = np.array(chop1)
+            chop2 = np.array(chop2)
+            chop3 = np.array(chop3)
+            chop4 = np.array(chop4)
 
 
-    matches1n = sorted(matches1n, key=lambda x: x.distance)
-    matches1n = matches1n[:50]
+            k1, d1 = sift.detectAndCompute(chop1, None)
+            k2, d2 = sift.detectAndCompute(chop2, None)
+            k1n, d1n = sift.detectAndCompute(chop3, None)
+            k2n, d2n = sift.detectAndCompute(chop4, None)
 
-    matches2 = sorted(matches2, key=lambda x: x.distance)
-    matches2 = matches2[:50]
 
-    matches2n = sorted(matches2n, key=lambda x: x.distance)
-    matches2n = matches2n[:50]
-    
+            bf = cv2.BFMatcher(cv2.NORM_L2, crossCheck=True)
+
+            #matches between imgl and imgln
+            matches1n = bf.match(d1,d1n)
+            #matches between imgl and imgr
+            matches2 = bf.match(d1,d2)
+            #matches between imgl and imgrn
+            matches2n = bf.match(d1,d2n)
+
+
+            matches1n = sorted(matches1n, key=lambda x: x.distance)
+            matches1n = matches1n[:50]
+
+            matches2 = sorted(matches2, key=lambda x: x.distance)
+            matches2 = matches2[:50]
+
+            matches2n = sorted(matches2n, key=lambda x: x.distance)
+            matches2n = matches2n[:50]
+
+            
+
+
+
+    #get the same matches
+    # same = new_matches & new_matches2
     
     #get the xy coordinate of the matches
-    left_matches = []
-    right_matches = []
-    left_matchesn =[]
-    right_matchesn = []
-    for match in matches1n:
-        x1, y1 = k1[match.queryIdx].pt
-        x1n, y1n = k1n[match.trainIdx].pt
+            left_matches = []
+            right_matches = []
+            left_matchesn =[]
+            right_matchesn = []
+            for match in matches1n:
+                    x1, y1 = k1[match.queryIdx].pt
+                    x1n, y1n = k1n[match.trainIdx].pt
+                    for match2 in matches2:
+                        if (match2.queryIdx == match.queryIdx):
+                            x2, y2 = k2[match2.trainIdx].pt
+                            left_matches.append(add_pixels(itr, x1, y1))
+                            left_matchesn.append(add_pixels(itr, x1n, y1n))
+                            right_matches.append(add_pixels(itr, x2, y2))
+                            for match2n in matches2n:
+                                if (match2n.queryIdx == match.queryIdx):
+                                    x2n, y2n = k2n[match2n.trainIdx].pt
+                                    right_matchesn.append(add_pixels(itr, x2n, y2n))
+                                    break
+                            break                  
+                    continue
 
-        for match2 in matches2:
-            if (match2.queryIdx == match.queryIdx):
-                x2, y2 = k2[match2.trainIdx].pt
-                for match2n in matches2n:
-                    if (match2n.queryIdx == match.queryIdx):
-                        x2n, y2n = k2n[match2n.trainIdx].pt
-                        left_matches.append([x1, y1])
-                        left_matchesn.append([x1n, y1n])
-                        right_matches.append([x2, y2])
-                        right_matchesn.append([x2n, y2n])
-                        break
-                break                  
-        continue
 
-    lmatch = np.array(left_matches)
-    lmatchn = np.array(left_matchesn)
-    rmatch = np.array(right_matches)
-    rmatchn = np.array(right_matchesn)
 
-    return lmatch, rmatch, lmatchn, rmatchn
+            lmatch = np.array(left_matches)
+            lmatchn = np.array(left_matchesn)
+            rmatch = np.array(right_matches)
+            rmatchn = np.array(right_matchesn) 
+
+            total_lmatches.append(lmatch)
+            total_rmatches.append(rmatch)
+            total_lmatchesn.append(lmatchn)
+            total_rmatchesn.append(rmatchn)
+
+            lmatches = np.array(total_lmatches)
+            rmatches = np.array(total_rmatches)
+            lmatchesn = np.array(total_lmatchesn)
+            rmatchesn = np.array(total_rmatchesn)
+
+            itr += 1
+
+
+
+    # shape = (90,2)
+    lmatches = np.concatenate(lmatches, axis = 0)
+    rmatches = np.concatenate(rmatches, axis = 0)
+    lmatchesn = np.concatenate(lmatchesn, axis = 0)
+    rmatchesn = np.concatenate(rmatchesn, axis = 0)
+
+
+
+
+    return lmatches, rmatches, lmatchesn, rmatchesn
+
+
+
+def add_pixels(itr, x,y):
+    if itr is 1:
+        x = x +320
+    elif itr is 2:
+        y = y + 240
+    elif itr is 3:
+        x = x+320
+        y = y+240
+    else:
+        x = x
+
+    return [x,y]
 
 

9d0761c2b529a7a26d83fd066016a7933bdaa4e4
yliang6 <yliang6@cs.brown.edu>
2020-05-05 19:29:16 -0400

    comments for main

--- main.py
+++ main.py
@@ -35,9 +35,11 @@ def main():
 	ic = io.collection.ImageCollection(data_dir+'/rectified/*.png')
 	half_idx = len(ic)//2
 	#load a few frames for testing
-	# left_ic = ic[0+513:500+513]
-	# right_ic = ic[half_idx+513:half_idx+500+513]
-	# ic = None
+	'''
+	left_ic = ic[0+513:500+513]
+	right_ic = ic[half_idx+513:half_idx+500+513]
+	ic = None
+	'''
 	left_ic = ic[:half_idx]
 	right_ic = ic[half_idx:]
 	# images have shape (480, 640, 3)
@@ -47,28 +49,26 @@ def main():
 	csv_feature_writer = csv.writer(csv_features_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
 	# initial camera pose
 	C = np.diag(np.ones(4))
+	# store the last valid frames in case of skipping frames
 	prevl = []
 	prevr = []
 	pts = np.array([])
 	for i in range(len(left_ic)-1):
 		print("Iteration ", i, "/", len(left_ic)-1)
+		# current frame
 		imgl1 = left_ic[i]
 		imgr1 = right_ic[i]
 		imgl2 = left_ic[i+1]
 		imgr2 = right_ic[i+1]
-		# display left and right images for verification purposes
-		'''
-		curr = [left_ic[i],right_ic[i]]
-		io.imshow_collection(curr)
-		plt.show()
-		'''
 		if PLOT.lower() == 'no_img' :
 			matchesl1, matchesr1, matchesl2, matchesr2 = match_features(imgl1, imgr1, imgl2, imgr2)
 		if PLOT.lower() == 'plot_img':
 			matchesl1, matchesr1, matchesl2, matchesr2 = match_features_plot(imgl1, imgr1, imgl2, imgr2)
 		if(matchesl1.shape[0] >= 3):	
+			# get matches
 			F, inliers_a1, inliers_b1, inliers_a2, inliers_b2,  = ransac_F_Matrix(matchesl1, matchesr1,matchesl2, matchesr2)
 			std_threshold = 2
+			# triangulate to find real-world points
 			coords3d1 = triangulate(inliers_a1, inliers_b1)
 			coords3d2 = triangulate(inliers_a2, inliers_b2)
 			if(len(coords3d1)==0 or len(coords3d2)==0):
@@ -83,21 +83,21 @@ def main():
 				pts = pts[:100]
 			mean = np.mean(pts)
 			std = np.std(pts)
-			# std1 = np.std(z1)
-			# std2 = np.std(z2)
+
 			mean1 = np.mean(z1)
 			mean2 = np.mean(z2)
 
-			# zstd1 = np.abs(std1-std)
-			# zstd2 = np.abs(std2-std)
 			zstd1 = mean1/std
 			zstd2 = mean2/std
 
 			print("std:", zstd1, zstd2)
+			# both frames k and k+1 invalid, skip
 			if(zstd1>=std_threshold and zstd2>=std_threshold):
 				print("skipping frame \n")
 				continue
+			# frame k invalid, frame k+1 valid
 			elif(zstd1>=std_threshold and zstd2<std_threshold):
+				# use last valid frame if exists, otherwise skip
 				if(prevl != [] and prevr != []):
 					matchesl1, matchesr1, matchesl2, matchesr2 = match_features(prevl, prevr, imgl2, imgr2)
 					F, inliers_a1, inliers_b1, inliers_a2, inliers_b2,  = ransac_F_Matrix(matchesl1, matchesr1,matchesl2, matchesr2)
@@ -106,11 +106,13 @@ def main():
 				else:
 					print("skipping frame \n")
 					continue
+			# frame k valid, frame k+1 invalid
 			elif(zstd1<std_threshold and zstd2>=std_threshold):
 				prevl = copy.deepcopy(imgl1)
 				prevr = copy.deepcopy(imgr1)
 				print("skipping frame \n")
 				continue
+			# both frames valid
 			else:
 				prevl = []
 				prevr = []

ee91f186e33fa3e703d85382894f4271342ef023
Alex Wey <53589584+alex-wey@users.noreply.github.com>
2020-05-05 02:30:43 -0400

    Update gif.py

--- gif.py
+++ gif.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import animation
 from mpl_toolkits.mplot3d import Axes3D
+!pip install ffmpeg-python
 
 # Input: numpy array of x,y,z coordinates of camera
 # Output: 3D representation of a camera's path as gif
@@ -11,25 +12,30 @@ def gif(matrix):
     Writer = animation.writers['ffmpeg']
     writer = Writer(fps=20, metadata=dict(artist='Me'), bitrate=1800, extra_args=['-vcodec', 'libx264'])
 
+    # plot actual points
+    actual_points = pd.read_csv("actual_points.csv").values
+    x_actual = actual_points[:,0]
+    z_actual = actual_points[:,1]
+
     # set up graphs
-    df = pd.DataFrame(matrix, columns=["x","y","z"])
+    df = pd.DataFrame(matrix, columns=["x","y"])
     frn = len(df)
     fig = plt.figure()
-    ax = fig.add_subplot(111,projection='3d')
-    sc = ax.scatter([],[],[], c='#d62728', alpha=0.5)
+    ax = fig.add_subplot(111)
+    ax.scatter(x_actual, z_actual, c='b')
+    sc = ax.scatter([],[], c='r', alpha=0.5)
     
     # label axis
     ax.set_title('Camera Pose')
-    ax.set_xlim3d(np.min(matrix[:,0])-10, np.max(matrix[:,0])+10)
+    ax.set_xlim(-10, 100)
     ax.set_xlabel('x')
-    ax.set_ylim3d(np.min(matrix[:,1])-10, np.max(matrix[:,1])+10)
+    ax.set_ylim(-45, 50)
     ax.set_ylabel('z')
-    ax.set_zlim3d(np.min(matrix[:,2])-10, np.max(matrix[:,2])+10)
-    ax.set_zlabel('y')
 
     def animate(i):
-        sc._offsets3d = (df.x.values[:i], df.y.values[:i], df.z.values[:i])
+        data = np.hstack((df.x.values[:i, np.newaxis], df.y.values[:i, np.newaxis]))
+        sc.set_offsets(data)
 
     # animate and save
-    ani = animation.FuncAnimation(fig, animate, frames=frn, interval=70)
+    ani = animation.FuncAnimation(fig, animate, frames=frn, interval=5)
     ani.save('SLAM.mp4', writer=writer)
